47e547c5aff9d82411fde843f7465c5e39da50ab
abz53378 <abz53378@gamil.com>
2020-11-05 00:20:19 +0800

    refactor onscroll api

--- components/GithubSeacher.tsx
+++ components/GithubSeacher.tsx
@@ -1,12 +1,16 @@
-import { useState, useCallback } from 'react';
+import { useState } from 'react';
 import { useSWRInfinite } from "swr";
-import { Input, Card, Typography} from 'antd';
-import {GithubOutlined} from '@ant-design/icons';
+import { Input, Card, Typography, Alert, Spin} from 'antd';
+import {GithubOutlined, LoadingOutlined} from '@ant-design/icons';
 import debounce from 'lodash.debounce';
 import List from "./List";
 const { Title, Paragraph } = Typography;
+const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
 
-const fetcher = url => fetch(url).then(res => res.json());
+const fetcher = url => fetch(url).then(res => {
+  if (!res.ok) throw res.statusText;
+  return res.json();
+});
 
 const renderItem = (item, i, style) => (
   <Card
@@ -21,30 +25,55 @@ const renderItem = (item, i, style) => (
 
 export default function GithubSeacher() {
   const [repo, setRepo] = useState("");
-  const { data = [], size, setSize } = useSWRInfinite(
-    index =>
-      `https://api.github.com/search/repositories?q=${repo}&page=${index +
-        1}`,
-    fetcher
+  const { data, error, size, isValidating, setSize } = useSWRInfinite(
+    index => {
+      if (!repo) return null;
+      return `https://api.github.com/search/repositories?q=${repo}&page=${index +
+        1}`
+    },
+    fetcher,
+    {
+      initialData: []
+    }
   );
-  console.log({data})
-  const onChange = useCallback(debounce((e) => {
-    console.log(e.target.value)
+  const onChange = debounce((e) => {
     setRepo(e.target.value);
-  }, 700), []);
-  const fetchMore = () => {
-    setSize(size + 1);
+    setSize(1); //reset size
+  }, 700);
+  const fetchMore = ({startIndex}: {startIndex: number}) => {
+    const currentSize = data.reduce((result, page) => result + page.items.length, 0);
+    if (startIndex > currentSize - 20 && !isValidating) {
+      setSize(size + 1);
+    }
   };
   return (
     <>
       <Input size="large" prefix={<GithubOutlined />} placeholder=" Input repository name" onChange={onChange} style={{ width: '100%' }}/>
-      <List
-        renderItem={renderItem}
-        items={data.map(item => item.items || []).flat()}
-        itemHeight={80}
-        visibleItemSize={4}
-        onScrollToBottom={fetchMore}
-      />
+      {
+        error ? (
+          <Alert
+            style={{marginTop: 24}}
+            message="Error"
+            description={error}
+            type="error"
+            showIcon
+          />
+        ) : (
+          <Spin
+            indicator={antIcon}
+            spinning={isValidating && size === 1}
+          >
+            <List
+              renderItem={renderItem}
+              items={data.map(item => item.items || []).flat()}
+              itemHeight={80}
+              windowHeight={320}
+              onScroll={fetchMore}
+            />
+          </Spin>
+        )
+      }
+     
     </>
   );
 }
--- components/List.tsx
+++ components/List.tsx
@@ -5,43 +5,41 @@ type Props<T extends Record<string, unknown>> = {
   renderItem: (item: T, index: number, style: React.CSSProperties) => React.ReactElement
   items: Array<T>,
   itemHeight: number;
-  visibleItemSize: number;
-  onScrollToBottom?: () =>void;
-  bottomOffset?: number;
+  windowHeight: number;
+  onScroll?: ({startIndex: number}) =>void;
 }
 
 export default function List<T extends Record<string, unknown>>(props: Props<T>) {
-  const {visibleItemSize, itemHeight, items, renderItem, onScrollToBottom, bottomOffset = 0} = props;
+  const {windowHeight, itemHeight, items, renderItem, onScroll} = props;
   const [index, setIndex] = useState(0);
-  const onScroll = e => {
+  const renderItemSize = Math.ceil(windowHeight / itemHeight) + 1;
+  const handleScroll = e => {
     const {
       scrollTop,
-      scrollHeight,
-      clientHeight,
     } = e.target;
-    const currentIndex = Math.floor(scrollTop / itemHeight);
-    setIndex(currentIndex);
-    if (scrollHeight - scrollTop - bottomOffset <= clientHeight ) {
-      onScrollToBottom && onScrollToBottom();
-    }
+    const startIndex = Math.floor(scrollTop / itemHeight);
+    setIndex(startIndex);
+    onScroll && onScroll({
+      startIndex
+    });
   }
 
   if (!items.length) {
-    return <Empty style={{padding: 64}} description={null}/>;
+    return <Empty style={{padding: 64, background: 'fff', margin: 0}} description={'No data'}/>;
   }
 
   return (
     <div style={{
-        height: visibleItemSize * itemHeight,
+        height: windowHeight,
         overflow: "auto"
       }}
-      onScroll={onScroll}
+      onScroll={handleScroll}
     >
       <div style={{
         position: "relative",
         height: items.length * itemHeight
       }}>
-        {items.slice(index, index + visibleItemSize + 1) // add one item size for buffer
+        {items.slice(index, index + renderItemSize)
           .map((item, i) => renderItem(item, index + i, {height: itemHeight, position: 'absolute', width: '100%', top: (index + i) * itemHeight}))
         }
       </div>
